0e3aba6ecdab00d47db36f73b8f3abad23c7ade6
avstechstar <avstechstar@gmail.com>
2020-03-29 09:37:59 -0500

    class 29 start using code -inside

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,7 +7,8 @@ namespace DatingApp.API.Data
     public class DataContext: DbContext // add this base class ,and install and reference to EF
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-
         public DbSet<Value> Values { get; set; }  // Values is table name
+        public DbSet<User> Users{ get; set;}
+
     }
 }
